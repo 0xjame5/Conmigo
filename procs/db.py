846464b16dc69354c4cd1e14ddc11980f7938f90
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-01-27 16:56:28 -0500

    add sentences and random sentence generator

---
+++ procs/db.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+KEYWORDS = {
+    "book": "http://inspirationalstorytellers.com/wp-content/uploads/2012/07/7035826-books-bokeh.jpg",
+    "box": "https://images-na.ssl-images-amazon.com/images/I/71hOwyFJxOL._SY355_.jpg",
+    "car": "https://www.enterprise.com/content/dam/global-vehicle-images/cars/VAUX_INSI_2014.png",
+    "cat": "https://d4n5pyzr6ibrc.cloudfront.net/media/27FB7F0C-9885-42A6-9E0C19C35242B5AC/4785B1C2-8734-405D-96DC23A6A32F256B/thul-90efb785-97af-5e51-94cf-503fc81b6940.jpg?response-content-disposition=inline",
+    "dog": "http://cdn2-www.dogtime.com/assets/uploads/2011/01/file_23244_what-is-the-appenzeller-sennenhunde-dog-300x189.jpg",
+    "house": "https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg",
+    "plane": "http://3.bp.blogspot.com/-pm4Qd6giL5A/VSGElbdFD6I/AAAAAAAAApU/8oaoGVQoAOc/s1600/Airlines%2BTracking.png",
+}
--- procs/translate.py
+++ procs/translate.py
@@ -1,18 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Imports the Google Cloud client library
 from google.cloud import translate
 
+from string import punctuation
+import random
+
+from db import KEYWORDS
+
 # Instantiates a client
-translate_client = translate.Client.from_service_account_json("key.json")
+translate_client = translate.Client.from_service_account_json(
+    "google_creds.json"
+)
 
-# The text to translate
-text = u'Wake me up WAKE ME UP INSIDE'
-# The target language
-target = 'es'
 
-# Translates some text into Russian
-translation = translate_client.translate(
-    text,
-    target_language=target)
+def translate_corpus(lang="es", new_corpus="sent_spanish_50.txt"):
 
-print(u'Text: {}'.format(text))
-print(u'Translation: {}'.format(translation['translatedText']))
+    data = []
+    with open("sent_english_50.txt") as sent_file:
+        for line in sent_file:
+            data.append(
+                translate_client.translate(
+                    line.replace("\n", ""),
+                    target_language=lang
+                )["translatedText"]
+            )
+
+    with open(new_corpus, "w") as new_corpus_file:
+        new_corpus_file.write("\n".join(data).encode("utf-8"))
+
+
+def get_random_sent():
+
+    with open("sent_english_50.txt") as sent_file:
+        sents = sent_file.read().split("\n")
+        chosen_sent = random.choice(sents)
+        normalized_sent = chosen_sent.translate(None, punctuation).split()
+        keyword = [i for i in normalized_sent if i in KEYWORDS.keys()][0]
+
+        return {
+            "sentence": chosen_sent,
+            "keyword": keyword,
+            "url": KEYWORDS[keyword],
+        }
+
+
+if __name__ == '__main__':
+
+    print get_random_sent()
+    translate_corpus()
