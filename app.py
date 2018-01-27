7b4ac115f20b7e2301ce0217e7b5f77cf86a642e
Jaime Orellana <jaime5@umbc.edu>
2018-01-27 02:10:01 -0500

    add certs

--- app.py
+++ app.py
@@ -364,4 +364,5 @@ def leave_rooms(newroom, username):
     return jsonify(success=True)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    context = ('cert.pem','key.pem')
+    app.run('0.0.0.0', ssl_context=context, debug=True)
